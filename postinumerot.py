7576b2cc4e6af452109523d038e310c20fc6ddb0
TommiVLavi <bgr912@myy.haaga-helia.fi>
2022-10-02 22:54:13 +0300

    Pytest Osa1 suoritettu

--- postinumerot.py
+++ postinumerot.py
@@ -1,1 +1,30 @@
-# Kopioi aikaisempi ratkaisusi tänne. Lisää tarvittaessa myös muut ratkaisusi tiedostot.
+import code
+import json
+from typing import Optional
+
+def get_postal_numbers(paikka: str) -> list:
+    with open('postinumerot.json') as material:
+        sisalto = material.read()
+
+    postinumerot = json.loads(sisalto)
+
+
+    
+    koodit = []
+
+    for koodi, nimi in postinumerot.items():
+        if nimi == paikka.upper():
+            koodit.append(koodi)
+    
+    if koodit:
+        koodit.sort()
+        print(', '.join(koodit))
+    else:
+        print("Tuntematon postitoimipaikka")
+
+    return koodit
+
+
+if __name__== '__main__':
+    paikka = input("Kirjoita postitoimipaikka: ")
+    get_postal_numbers(paikka)
--- postinumerot_test.py
+++ postinumerot_test.py
@@ -1,1 +1,20 @@
 # Kirjoita postinumerot-moduulin testit tähän tiedostoon
+ 
+# postitoimipaikan nimellä löytyy vain yksi postinumero (Muuruvesi)
+# postitoimipaikan nimellä löytyy useita postinumeroita
+# postitoimipaikan nimellä ei löydy lainkaan postinumeroita.
+
+from postinumerot import get_postal_numbers
+
+def test_get_one_num_from_name():
+    paikka = input("Antaisitko postitoimipaikan: ")
+    assert get_postal_numbers(paikka) == ["73460"]
+
+def test_get_several_numbers():
+    paikka = input("Antaisitko postitoimipaikan: ")
+    lista = get_postal_numbers(paikka)
+    assert len(lista) >= 2
+
+def test_get_absolutely_none():
+    paikka = input("Antaisitko postitoimipaikan: ")
+    assert get_postal_numbers(paikka) == []
